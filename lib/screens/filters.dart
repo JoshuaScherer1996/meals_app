bfff7311da0ac274b8f62ef83e8d973e67e66229
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-01-27 14:43:48 +0100

    finished the navigation between screens while all the navigations are still available in the stack so that the user can use the back button to undo the navigations.

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:meals_app/main.dart';
+import 'package:meals_app/screens/tabs.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
 
 class FiltersScreen extends StatefulWidget {
   const FiltersScreen({super.key});
@@ -18,6 +19,18 @@ class _FiltersScreenState extends State<FiltersScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Filters'),
+      ),
+      drawer: MainDrawer(
+        onSelectScreen: (identifier) {
+          Navigator.of(context).pop();
+          if (identifier == 'meals') {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (ctx) => const TabsScreen(),
+              ),
+            );
+          }
+        },
       ),
       body: Column(
         children: [
