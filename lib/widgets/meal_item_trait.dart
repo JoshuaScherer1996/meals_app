2eb9d297ba3a74dca066c9eb568f01cfb5f7c7c4
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-01-30 14:51:46 +0100

    added documentation meal_item_trait.dart.

--- lib/widgets/meal_item_trait.dart
+++ lib/widgets/meal_item_trait.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 
+// MealItemTrait: A reusable stateless widget to display an icon and a label.
 class MealItemTrait extends StatelessWidget {
-  const MealItemTrait({super.key, required this.icon, required this.label});
+  // Constructor for MealItemTrait.
+  // It requires two parameters: 'icon' for the IconData and 'label' for the text to be displayed.
+  const MealItemTrait({
+    super.key,
+    required this.icon,
+    required this.label,
+  });
 
+  // Declaring the icon data to be used in the widget.
   final IconData icon;
+  // Declaring the label text to be displayed next to the icon.
   final String label;
 
+  // Building the UI of the MealItemTrait widget.
   @override
   Widget build(BuildContext context) {
+    // Using a Row to arrange the icon and text label horizontally.
     return Row(
       children: [
+        // Displaying the icon.
         Icon(
           icon,
           size: 17,
@@ -18,6 +30,7 @@ class MealItemTrait extends StatelessWidget {
         const SizedBox(
           width: 6,
         ),
+        // Displaying the label text.
         Text(
           label,
           style: const TextStyle(
