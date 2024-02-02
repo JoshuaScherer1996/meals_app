bd2e86ee12630d67220c944181b5d624b8b150c3
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-02-02 21:47:00 +0100

    added documentation to filters_provider.

--- lib/providers/filters_provider.dart
+++ lib/providers/filters_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meals_app/providers/meals_provider.dart';
 
+// Define an enumeration 'Filter' with four types of dietary restrictions.
 enum Filter {
   glutenFree,
   lactoseFree,
@@ -8,7 +9,10 @@ enum Filter {
   vegan,
 }
 
+// Declare 'FiltersNotifier' class that extends 'StateNotifier' with a state of type Map<Filter, bool>.
+// This class manages the state of dietary filters.
 class FiltersNotifier extends StateNotifier<Map<Filter, bool>> {
+  // Initialize the state with all filters set to false indicating that no dietary filters are applied initially.
   FiltersNotifier()
       : super({
           Filter.glutenFree: false,
@@ -17,10 +21,13 @@ class FiltersNotifier extends StateNotifier<Map<Filter, bool>> {
           Filter.vegan: false
         });
 
+  // Method to update the state with a new set of chosen filters.
   void setFilters(Map<Filter, bool> chosenFilters) {
     state = chosenFilters;
   }
 
+  // Method to update the state for a single filter.
+  // This allows toggling a specific filter on or off.
   void setFilter(Filter filter, bool isActive) {
     state = {
       ...state,
@@ -29,16 +36,23 @@ class FiltersNotifier extends StateNotifier<Map<Filter, bool>> {
   }
 }
 
+// Provider 'filtersProvider' is defined to manage the filters state.
+// It creates an instance of 'FiltersNotifier', which holds the state of dietary filters.
 final filtersProvider =
     StateNotifierProvider<FiltersNotifier, Map<Filter, bool>>(
   (ref) => FiltersNotifier(),
 );
 
+// Provider 'filteredMealsProvider' is defined to filter meals based on active dietary filters.
+// It watches 'mealsProvider' for the list of meals and 'filtersProvider' for the active filters.
 final filteredMealsProvider = Provider((ref) {
   final meals = ref.watch(mealsProvider);
   final activeFilters = ref.watch(filtersProvider);
 
+  // Filter the list of meals based on the active dietary filters.
   return meals.where((meal) {
+    // Apply each filter by checking if the filter is active and if the meal complies with the dietary restriction.
+    // Returns false to exclude the meal if it does not comply with an active filter.
     if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
       return false;
     }
@@ -51,6 +65,6 @@ final filteredMealsProvider = Provider((ref) {
     if (activeFilters[Filter.vegan]! && !meal.isVegan) {
       return false;
     }
-    return true;
-  }).toList();
+    return true; // Include the meal if it passes all active filters.
+  }).toList(); // Convert the filtered iterable back into a list.
 });
