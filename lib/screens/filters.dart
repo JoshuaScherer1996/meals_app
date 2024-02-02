a804e13722b1dc0379a0dc44c845ea54257f564a
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-02-02 23:06:49 +0100

    added documentation to filters screen.

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -2,27 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/providers/filters_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// FiltersScreen class that represents a screen with dietary filter options.
+// FiltersScreen class that represents a screen within the app where users can select dietary filters.
 class FiltersScreen extends ConsumerWidget {
-  // Constructor for FiltersScreen.
+  // Constructor for FiltersScreen, accepting a key for widget identification.
   const FiltersScreen({super.key});
 
-  // Building the UI of the FiltersScreen.
+  // Overriding the build method to define the UI structure of the FiltersScreen.
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // Accessing the current state of dietary filters using Riverpod's watch method.
     final activeFilters = ref.watch(filtersProvider);
 
+    // Building the UI layout with Scaffold for app structure.
     return Scaffold(
+      // AppBar at the top displaying the screen title.
       appBar: AppBar(
         title: const Text('Your Filters'),
       ),
+      // Body containing a list of switchable options for dietary filters.
       body: Column(
-        // List of SwitchListTiles to enable/disable filters.
         children: [
-          // Each SwitchListTile corresponds to a dietary filter.
+          // Each SwitchListTile represents a toggleable option for a dietary filter.
+          // Below, four filters are defined: Gluten-free, Lactose-free, Vegetarian, and Vegan.
+
+          // Gluten-free filter switch.
           SwitchListTile(
-            value: activeFilters[Filter.glutenFree]!,
+            value: activeFilters[Filter
+                .glutenFree]!, // The current state of the gluten-free filter.
             onChanged: (isChecked) {
+              // Updates the filter state when the switch is toggled.
               ref
                   .read(filtersProvider.notifier)
                   .setFilter(Filter.glutenFree, isChecked);
@@ -42,6 +50,7 @@ class FiltersScreen extends ConsumerWidget {
             activeColor: Theme.of(context).colorScheme.tertiary,
             contentPadding: const EdgeInsets.only(left: 34, right: 22),
           ),
+          // The same structure is repeated for Lactose-free, Vegetarian, and Vegan filters
           SwitchListTile(
             value: activeFilters[Filter.lactoseFree]!,
             onChanged: (isChecked) {
